1360a214fdd574da3295ae188cc48316ed3359b8
Sebastien Cayetanot <sebastien.cayetanot@icloud.com>
2023-01-27 19:58:07 -0800

    Splash screen fragment and transition to blank home

--- app/src/main/java/com/whatevrdev/mercedesyelp/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/whatevrdev/mercedesyelp/ui/SplashScreenFragment.kt
@@ -1,24 +1,29 @@
 package com.whatevrdev.mercedesyelp.ui
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.whatevrdev.mercedesyelp.R
 
 class SplashScreenFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //Just a simple delay to show the splash but we can add some logic if required
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
+        }, 3000)
     }
 
 }
